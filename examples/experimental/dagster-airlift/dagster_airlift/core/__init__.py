eb5965f3f7d558cad964888289339a38a2c74147
Nick Schrock <schrockn@dagsterlabs.com>
2024-08-09 15:03:53 -0700

    Export DefsFactory at dagster_airlift.def_factory

--- examples/experimental/dagster-airlift/dagster_airlift/core/__init__.py
+++ examples/experimental/dagster-airlift/dagster_airlift/core/__init__.py
@@ -1,4 +1,5 @@
 from .basic_auth import BasicAuthBackend as BasicAuthBackend
+from .def_factory import DefsFactory as DefsFactory
 from .defs_from_airflow import (
     AirflowInstance as AirflowInstance,
     build_defs_from_airflow_instance as build_defs_from_airflow_instance,
